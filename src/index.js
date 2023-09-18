f9a9fe84e6adb2223441c802206ceb3229d533e6
hall488 <hall488@purdue.edu>
2023-09-17 21:28:53 -0400

    Add Up and Right rotations
    Turn handles moving the pieces from each face to the next.
    Rotate handles moving pieces around the axis face.

--- src/index.js
+++ src/index.js
@@ -5,12 +5,89 @@ const pan = Pan();
 
 const colors = [".red", ".blue", ".green", ".yellow", ".orange", ".white"];
 
-colors.forEach((c) => {
-  let divs = document.querySelectorAll(c);
+const front = document.querySelector(".front");
+const right = document.querySelector(".right");
+const back = document.querySelector(".back");
+const left = document.querySelector(".left");
+const top = document.querySelector(".top");
+const down = document.querySelector(".down");
 
-  divs.forEach((d) => {
-    d.addEventListener("click", () => {
-      console.log(d.classList);
-    });
-  });
+const moves = {
+  U: [
+    { face: front, from: [0, 1, 2], to: [0, 1, 2] },
+    { face: left, from: [0, 1, 2], to: [0, 1, 2] },
+    { face: back, from: [0, 1, 2], to: [2, 1, 0] },
+    { face: right, from: [0, 1, 2], to: [2, 1, 0] },
+  ],
+  R: [
+    { face: front, from: [2, 5, 8], to: [2, 5, 8] },
+    { face: top, from: [2, 5, 8], to: [2, 6, 10] },
+    { face: back, from: [2, 5, 8], to: [10, 6, 2] },
+    { face: down, from: [2, 5, 8], to: [10, 6, 2] },
+  ],
+};
+
+document.addEventListener("keypress", (e) => {
+  switch (e.key) {
+    case "u":
+      turn(moves.U);
+      break;
+    case "r":
+      turn(moves.R);
+      break;
+  }
 });
+
+// colors.forEach((c) => {
+//   let divs = document.querySelectorAll(c);
+
+//   divs.forEach(() => {
+
+//   });
+// });
+
+const turn = (move) => {
+  let order = move;
+
+  let toBeMoved = [];
+
+  order.forEach((o) => {
+    let divs = [];
+
+    o.from.forEach((p) => divs.push(o.face.children[p]));
+
+    toBeMoved.push(divs);
+  });
+
+  console.log(toBeMoved);
+
+  for (let i = 0; i < 4; i++) {
+    let tbm = toBeMoved[i];
+
+    let o = order[(i + 1) % 4];
+
+    for (let j = 0; j < tbm.length; j++) {
+      console.log(tbm[j], o.from[j], o.face.children[o.to[j]]);
+
+      o.face.insertBefore(tbm[j], o.face.children[o.to[j]]);
+    }
+  }
+
+  switch (move) {
+    case moves.R:
+      rotate(right);
+      break;
+    case moves.U:
+      rotate(top);
+      break;
+  }
+};
+
+const rotate = (face, direction) => {
+  let n = [...face.children];
+  face.innerHTML = "";
+  let rotation = [n[6], n[3], n[0], n[7], n[4], n[1], n[8], n[5], n[2]];
+  rotation.forEach((r) => {
+    face.append(r);
+  });
+};
